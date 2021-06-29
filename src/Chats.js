caeb8367e2542286d091f0fc87620e239dd9cb4e
sudeepbhandari6545 <sudeepbhandari6545@gmail.com>
2021-06-29 15:29:14 +0545

    snapchat

--- src/Chats.js
+++ src/Chats.js
@@ -1,8 +1,24 @@
 import React from 'react'
+//icons
+import { Avatar } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble'
 import './Chats.css'
 
 function Chats() {
-  return <div className="chats"></div>
+  return (
+    <div className="chats">
+      <div className="chats_header">
+        <Avatar className="chats_avatar" />
+        <div className="chats_search">
+          <SearchIcon />
+          <input type="text" placeholder="friends" />
+        </div>
+        <ChatBubbleIcon className="chat_bubbleicon" />
+      </div>
+      <div className="chat_post"></div>
+    </div>
+  )
 }
 
 export default Chats
